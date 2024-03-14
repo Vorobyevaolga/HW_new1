d05c59cf308ba187161a413623f89de7bb48347b
MI <Bocha555olga@yandex.ru>
2024-03-14 21:58:31 +0400

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
 public class Main {
-    public static void main(String[] args) {  System.out.println("Hello world1!");
+    public static void main(String[] args) {  System.out.println("Hello world!");
     }
 }
